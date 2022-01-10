dcfa4c47e64720636292df996bc9ddab979f154e
sergio2305 <sergio230564@gmail.com>
2022-01-10 14:52:13 -0300

    desafio bds06 final

---
+++ src/main/java/com/devsuperior/movieflix/repositories/ReviewRepository.java
@@ -0,0 +1,19 @@
+package com.devsuperior.movieflix.repositories;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.devsuperior.movieflix.entities.Review;
+import com.devsuperior.movieflix.entities.User;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+	@Query("SELECT obj FROM Review obj WHERE movie_id = :movieId")
+	List<Review> findReviewsOfMovie(Long movieId);
+	
+	Page<Review> findByUser(User user, Pageable pageable);
+}
---
+++ src/main/java/com/devsuperior/movieflix/services/GenreService.java
@@ -0,0 +1,31 @@
+package com.devsuperior.movieflix.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.movieflix.dto.GenreDTO;
+import com.devsuperior.movieflix.entities.Genre;
+import com.devsuperior.movieflix.repositories.GenreRepository;
+
+@Service
+public class GenreService 
+{
+
+	private static Logger logger = LoggerFactory.getLogger(GenreService.class);
+
+	@Autowired
+	private GenreRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<GenreDTO> findAll() {
+		List<Genre> list = repository.findAll();
+		
+		return list.stream().map(x -> new GenreDTO(x)).collect(Collectors.toList());
+	}
+}
